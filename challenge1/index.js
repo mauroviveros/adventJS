62fce9c8f0045b0ccfd594ad022edb147559ea92
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-12-01 14:31:42 -0300

    feat: :sparkles: second try to first challenge

--- challenge1/index.js
+++ challenge1/index.js
@@ -3,3 +3,12 @@ function findFirstRepeated(gifts) {
   const reps = gifts.filter((gift, index) => gifts.indexOf(gift) !== index);
   return reps.length ? reps[0] : -1;
 }
+
+// 160 Points
+function findFirstRepeated(gifts) {
+
+  for(const [index, gift] of gifts.entries()){
+    if(gifts.indexOf(gift) !== index) return gift;
+  }
+  return -1;
+}
