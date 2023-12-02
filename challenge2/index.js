d5408795998d0c029e35fa427e9ecdc23dd9c8d6
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-12-02 16:31:41 -0300

    feat: :sparkles: add ChatGPT solution for challenge 2

--- challenge2/index.js
+++ challenge2/index.js
@@ -4,3 +4,37 @@ function manufacture(gifts, materials) {
     return gift.split("").every(char => materials.includes(char))
   });
 }
+
+
+
+// ChatGPT Solution - 260 Points
+function manufacture(gifts, materials) {
+  // Función auxiliar para verificar si se pueden fabricar los regalos
+  function canManufactureGift(gift, materials) {
+    // Iterar sobre cada caracter del regalo
+    for (const char of gift) {
+      // Verificar si el caracter no está en los materiales
+      if (!materials.includes(char)) {
+        // Si un caracter no está presente, el regalo no se puede fabricar
+        return false;
+      }
+    }
+
+    // Si todos los caracteres están presentes, el regalo se puede fabricar
+    return true;
+  }
+  // Crear un array para almacenar los regalos fabricados
+  const manufacturedGifts = [];
+
+  // Iterar sobre cada regalo en la lista
+  for (const gift of gifts) {
+    // Verificar si todos los caracteres del regalo están en los materiales
+    if (canManufactureGift(gift, materials)) {
+      // Si es así, agregar el regalo a la lista de regalos fabricados
+      manufacturedGifts.push(gift);
+    }
+  }
+
+  // Devolver la lista de regalos fabricados
+  return manufacturedGifts;
+}
