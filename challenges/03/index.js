61f2746b58f697f83e756c93a18211340465aebe
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-12-04 11:33:07 -0300

    feat: :sparkles: add second solution for challenge 3

--- challenges/03/index.js
+++ challenges/03/index.js
@@ -1,4 +1,4 @@
-// 280 Points ⚠️ It's hard to read.
+// 280 Points ⚠️ It's hard to read
 function findNaughtyStep(original, modified) {
   let naughty = "";
   if(original === modified) return naughty;
@@ -6,5 +6,14 @@ function findNaughtyStep(original, modified) {
 
   for(let i = 0; i < length; i++){
     if(original[i] !== modified[i]) return naughty += length === original.length ? original[i] : modified[i];
+  }
+}
+
+// 250 Points 
+function findNaughtyStep(original, modified) {
+  if(original === modified) return "";
+  let wrapper = [original, modified].sort((a, b) => b.length - a.length);
+  for(let i = 0; i < wrapper[0].length; i++){
+    if(wrapper[0][i] !== wrapper[1][i]) return wrapper[0][i];
   }
 }
