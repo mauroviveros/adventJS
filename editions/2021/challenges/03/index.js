4669c58e7edeff132c47dc63d5bf670238ce079b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-12 15:18:42 -0300

    feat(2021): :sparkles: add solution challenge 02

---
+++ editions/2021/challenges/03/index.js
@@ -0,0 +1,14 @@
+export default function isValid(letter) {
+    const gifts = letter.match(/\((.*?)(\)|$)/gm);
+    if (gifts === null) return true;
+
+    const invalidGift = gifts.find(gift => {
+      return (
+        gift.length === 2 ||
+        gift.slice(-1) !== ')' ||
+        gift.slice(1, -1).match(/[(){}[\]]/)
+      );
+    });
+
+    return invalidGift === undefined;
+}
