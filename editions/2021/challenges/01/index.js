addb0ac34eee65ce1c04d255cb1f553b43932791
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-15 08:50:37 -0300

    fix(2021): :rotating_light: add editorconfig

--- editions/2021/challenges/01/index.js
+++ editions/2021/challenges/01/index.js
@@ -1,6 +1,6 @@
 export default function contarOvejas(ovejas) {
-    return ovejas.filter(oveja => {
-        const name = oveja.name.toLowerCase();
-        return oveja.color === "rojo" && name.includes("n") && name.includes("a");
-    });
+  return ovejas.filter(oveja => {
+    const name = oveja.name.toLowerCase();
+    return oveja.color === "rojo" && name.includes("n") && name.includes("a");
+  });
 };
--- editions/2021/challenges/03/index.js
+++ editions/2021/challenges/03/index.js
@@ -1,14 +1,14 @@
 export default function isValid(letter) {
-    const gifts = letter.match(/\((.*?)(\)|$)/gm);
-    if (gifts === null) return true;
+  const gifts = letter.match(/\((.*?)(\)|$)/gm);
+  if (gifts === null) return true;
 
-    const invalidGift = gifts.find(gift => {
-      return (
-        gift.length === 2 ||
-        gift.slice(-1) !== ')' ||
-        gift.slice(1, -1).match(/[(){}[\]]/)
-      );
-    });
+  const invalidGift = gifts.find(gift => {
+    return (
+      gift.length === 2 ||
+      gift.slice(-1) !== ')' ||
+      gift.slice(1, -1).match(/[(){}[\]]/)
+    );
+  });
 
-    return invalidGift === undefined;
+  return invalidGift === undefined;
 }
