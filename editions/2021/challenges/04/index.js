fe8fbbf344e07cf36df33e98fe1b6c092f9b504b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-15 08:50:53 -0300

    feat(2021): :sparkles: add solution for challenge 04

---
+++ editions/2021/challenges/04/index.js
@@ -0,0 +1,15 @@
+function createXmasTree(height) {
+  const lines = [];
+  if (height <= 0) return "";
+  const createLine = (char, width) => {
+    return `${"_".repeat(height - width)}${char.repeat(width + width - 1)}${"_".repeat(height - width)}`;
+
+  }
+
+  for (let i = 1; i <= height; i++) {
+    lines.push(createLine("*", i));
+  }
+  lines.push(createLine("#", 1));
+  lines.push(createLine("#", 1));
+  return lines.join("\n");
+}
