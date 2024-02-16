d590074faaf21fd5b6a47b501e67ef622dca0a2f
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-15 23:23:04 -0300

    fix(2021): :adhesive_bandage: fix solution on challenge 04

--- editions/2021/challenges/04/index.js
+++ editions/2021/challenges/04/index.js
@@ -1,15 +1,16 @@
-function createXmasTree(height) {
+export default function createXmasTree(height) {
   const lines = [];
-  if (height <= 0) return "";
+  if (height <= 0) return '';
   const createLine = (char, width) => {
-    return `${"_".repeat(height - width)}${char.repeat(width + width - 1)}${"_".repeat(height - width)}`;
-
-  }
+    return `${'_'.repeat(height - width)}${char.repeat(
+      width + width - 1
+    )}${'_'.repeat(height - width)}`;
+  };
 
   for (let i = 1; i <= height; i++) {
-    lines.push(createLine("*", i));
+    lines.push(createLine('*', i));
   }
-  lines.push(createLine("#", 1));
-  lines.push(createLine("#", 1));
-  return lines.join("\n");
+  lines.push(createLine('#', 1));
+  lines.push(createLine('#', 1));
+  return lines.join('\n');
 }
