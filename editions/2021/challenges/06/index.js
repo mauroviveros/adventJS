b422be3f20b34be003deb7c545545acb8bebbf2a
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-17 14:53:39 -0300

    fix(2021): :hammer: add nother solution for challenge 06

--- editions/2021/challenges/06/index.js
+++ editions/2021/challenges/06/index.js
@@ -1,11 +1,22 @@
-export default function sumPairs(numbers, result) {
-  const pairs = numbers.map((num, i) => {
-    const pair = numbers.find((num2, i2) => {
-      return i != i2 && num + num2 == result;
-    });
-    if (pair) return [num, pair];
-    return null;
-  });
+// export default function sumPairs(numbers, result) {
+//   const pairs = numbers.map((num, i) => {
+//     const pair = numbers.find((num2, i2) => {
+//       return i != i2 && num + num2 == result;
+//     });
+//     if (pair) return [num, pair];
+//     return null;
+//   });
 
-  return pairs.find((pair) => pair) || null;
+//   return pairs.find((pair) => pair) || null;
+// }
+
+export default function sumPairs(numbers, result) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === result) {
+        return [numbers[i], numbers[j]];
+      }
+    }
+  }
+  return null;
 }
