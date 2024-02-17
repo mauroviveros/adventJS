cc7fdadb625f7eaa6b6edafc8b04fb9eee9ee5f5
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-17 15:05:12 -0300

    fix(2021): 🔨 add nother solution for challenge 07

--- editions/2021/challenges/07/index.js
+++ editions/2021/challenges/07/index.js
@@ -1,9 +1,13 @@
-export default function contains(store, product) {
-  if (typeof store === 'string') return store === product;
+// export default function contains(store, product) {
+//   if (typeof store === 'string') return store === product;
 
-  for (let bookshelf in store) {
-    const bool = contains(store[bookshelf], product);
-    if (bool) return true;
-  }
-  return false;
+//   for (let bookshelf in store) {
+//     const bool = contains(store[bookshelf], product);
+//     if (bool) return true;
+//   }
+//   return false;
+// }
+
+export default function contains(store, product) {
+  return new RegExp(`:"${product}"`, 'i').test(JSON.stringify(store));
 }
