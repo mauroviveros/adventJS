e512e92e5cd7ee79dbbe539917ee9b3e7199fb65
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-17 15:15:39 -0300

    fix(2021): 🔨 add nother solution for challenge 02

--- editions/2021/challenges/02/index.js
+++ editions/2021/challenges/02/index.js
@@ -1,9 +1,21 @@
+// export default function listGifts(letter) {
+//   const gifts = {};
+//   letter.trim()
+//     .replace(/\s{2,}/g, ' ')
+//     .split(" ")
+//     .filter(gift => gift[0] !== "_")
+//     .forEach(gift => gifts[gift] = gifts[gift] ? gifts[gift] + 1 : 1);
+//   return gifts;
+// }
+
 export default function listGifts(letter) {
-  const gifts = {};
-  letter.trim()
-    .replace(/\s{2,}/g, ' ')
-    .split(" ")
-    .filter(gift => gift[0] !== "_")
-    .forEach(gift => gifts[gift] = gifts[gift] ? gifts[gift] + 1 : 1);
-  return gifts;
+  return letter
+    .trim()
+    .split(' ')
+    .filter((gift) => !gift.startsWith('_'))
+    .reduce((result, gift) => {
+      if (!result[gift]) result[gift] = 0;
+      result[gift]++;
+      return result;
+    }, {});
 }
