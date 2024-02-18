98de8ccecc19fc9fa515cf511a68921a0ce7082b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-18 17:11:32 -0300

    fix(2021): 🔨 add another solution for challenge 04

--- editions/2021/challenges/04/index.js
+++ editions/2021/challenges/04/index.js
@@ -1,3 +1,28 @@
+// export default function createXmasTree(height) {
+//   const SYMBOL = {
+//     LEAF: '*',
+//     GAP: '_',
+//     TRUNK: '#',
+//   }
+//   const tree = [];
+//   let width = 1;
+//   let emptyChars = height - 1;
+//   let gap = SYMBOL.GAP.repeat(emptyChars);
+//   const trunk = gap + SYMBOL.TRUNK + gap;
+
+//   for (let i = 1; i <= height; i++) {
+//     let leafs = SYMBOL.LEAF.repeat(width);
+//     gap = SYMBOL.GAP.repeat(emptyChars);
+//     tree.push(gap + leafs + gap);
+//     width += 2;
+//     emptyChars--;
+//   }
+//   tree.push(trunk);
+//   tree.push(trunk);
+
+//   return tree.join('\n');
+// }
+
 export default function createXmasTree(height) {
   const lines = [];
   if (height <= 0) return '';
