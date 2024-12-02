4c31b96a8aa531f856008fd75fc95d4aceb54860
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-12-02 13:33:57 -0300

    fix(2024): :hammer: add another solution challengee 02

--- editions/2024/challenges/02/index.js
+++ editions/2024/challenges/02/index.js
@@ -1,3 +1,5 @@
+// 2297 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 3 (Más bajo es mejor)
 function createFrame(names) {
   const maxLength = Math.max(...names.map((n) => n.length));
   names = names.map((n) => {
@@ -8,4 +10,31 @@ function createFrame(names) {
   names.push('*'.repeat(maxLength + 4));
 
   return names.join('\n');
+}
+
+// 2130 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 3 (Más bajo es mejor)
+function createFrame(names) {
+  const maxLength = Math.max(...names.map((n) => n.length));
+  names = names.map((n) => {
+    const spaces = ' '.repeat(maxLength - n.length);
+    return `* ${n}${spaces} *`;
+  });
+
+  return ['*'.repeat(maxLength + 4), ...names, '*'.repeat(maxLength + 4)].join(
+    '\n'
+  );
+}
+
+// 2393 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 3 (Más bajo es mejor)
+function createFrame(names) {
+  const length = Math.max(...names.map((n) => n.length));
+  const formatName = (n) => `* ${n.padEnd(length)} *`;
+
+  return [
+    '*'.repeat(length + 4),
+    ...names.map(formatName),
+    '*'.repeat(length + 4),
+  ].join('\n');
 }
