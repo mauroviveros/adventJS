56af4975f7d1885ee98f3e7bdfb397d1a30c94e8
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-12-14 16:41:29 -0300

    feat(2024): :sparkles: add solution 06

---
+++ editions/2024/challenges/06/index.js
@@ -0,0 +1,15 @@
+// ⭐️⭐️⭐️⭐️⭐️
+// 2398 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 3
+/** @param {string[]} box
+ *  @returns {boolean} True if the gift is inside the box
+ */
+function inBox(box) {
+  for (let i = 1; i < box.length - 1; i++) {
+    const line = box[i].substring(1, box[i].length - 1);
+    const iGift = line.indexOf('*');
+    if (iGift != -1) return true;
+  }
+
+  return false;
+}
