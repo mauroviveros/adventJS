09e611b558798b6116a660edc61142e25d0dde30
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-14 23:12:25 -0300

    test(2021): :test_tube: add testings for challenge 16

--- editions/2021/challenges/16/index.test.js
+++ editions/2021/challenges/16/index.test.js
@@ -1,14 +1,55 @@
 import decodeNumbers from './index.js';
 
-console.log(decodeNumbers('...')); // 3
-console.log(decodeNumbers('.,')); // 4 (5 - 1)
-console.log(decodeNumbers(',.')); // 6 (5 + 1)
-console.log(decodeNumbers(',...')); // 8 (5 + 3)
-console.log(decodeNumbers('.........!')); // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
-console.log(decodeNumbers('.;')); // 49 (50 - 1)
-console.log(decodeNumbers('..,')); // 5 (-1 + 1 + 5)
-console.log(decodeNumbers('..,!')); // 95 (1 - 1 - 5 + 100)
-console.log(decodeNumbers('.;!')); // 49 (-1 -50 + 100)
-console.log(decodeNumbers('!!!')); // 300
-console.log(decodeNumbers(';!')); // 50
-console.log(decodeNumbers(';.W')); // NaN
+describe('Challenge 16 - 2021', () => {
+  test('Test #01 - expected return type', () => {
+    expect(typeof decodeNumbers('') === 'number').toBe(true);
+  });
+
+  test(`Test #02 - expected decodeNumbers('...')`, () => {
+    expect(decodeNumbers('...')).toBe(3);
+  });
+
+  test(`Test #03 - expected decodeNumbers('.,')`, () => {
+    expect(decodeNumbers('.,')).toBe(4); // 5 - 1
+  });
+
+  test(`Test #04 - expected decodeNumbers(',.')`, () => {
+    expect(decodeNumbers(',.')).toBe(6); // 5 + 1
+  });
+
+  test(`Test #05 - expected decodeNumbers(',...')`, () => {
+    expect(decodeNumbers(',...')).toBe(8); // 5 + 3
+  });
+
+  test(`Test #06 - expected decodeNumbers('.........!')`, () => {
+    expect(decodeNumbers('.........!')).toBe(107); // (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+  });
+
+  test(`Test #07 - expected decodeNumbers('.;')`, () => {
+    expect(decodeNumbers('.;')).toBe(49); // 50 - 1
+  });
+
+  test(`Test #08 - expected decodeNumbers('..,')`, () => {
+    expect(decodeNumbers('..,')).toBe(5); // (-1 + 1 + 5)
+  });
+
+  test(`Test #09 - expected decodeNumbers('..,!')`, () => {
+    expect(decodeNumbers('..,!')).toBe(95); // (1 - 1 - 5 + 100)
+  });
+
+  test(`Test #10 - expected decodeNumbers('.;!')`, () => {
+    expect(decodeNumbers('.;!')).toBe(49); // (-1 -50 + 100)
+  });
+
+  test(`Test #11 - expected decodeNumbers('!!!')`, () => {
+    expect(decodeNumbers('!!!')).toBe(300);
+  });
+
+  test(`Test #12 - expected decodeNumbers(';!')`, () => {
+    expect(decodeNumbers(';!')).toBe(50);
+  });
+
+  test(`Test #13 - expected decodeNumbers(';.W')`, () => {
+    expect(decodeNumbers(';.W')).toBeNaN();
+  });
+});
